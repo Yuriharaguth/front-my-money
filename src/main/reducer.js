de100acb37224b20c460f7a26d3b88946b88cbd3
Yuri Haraguth <haraguths@gmail.com>
2017-09-13 22:24:45 -0300

    Action reducer ABAS

--- src/main/reducer.js
+++ src/main/reducer.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux'
 
 import DashBoardReducer from '../dashboard/dashboardReducer'
+import TabReducer from '../common/tab/tabReducer'
 
 const rootReducer = combineReducers({
-    dashBoard: DashBoardReducer
+    dashBoard: DashBoardReducer,
+    tab: TabReducer
 })
 
 export default rootReducer
