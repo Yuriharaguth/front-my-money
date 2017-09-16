fdec476c956d8b71e2f67606756fcf62d9426182
Yuri Haraguth <haraguths@gmail.com>
2017-09-16 18:13:35 -0300

    Listagem de pagamentos parte 1

--- src/billingCycle/billingCyclesActions.js
+++ src/billingCycle/billingCyclesActions.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
-const BASE_URL = 'http://localhost:300/api'
+const BASE_URL = 'http://localhost:3003/api'
 
 export function getList() {
-    const request = axions.get(`${BASE_URL}/billingCycles`)
+    const request = axios.get(`${BASE_URL}/billingCycles`)
     return {
         type: 'BILLING_CYCLES_FETCHED',
         payload: request
