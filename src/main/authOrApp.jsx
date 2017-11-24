defad04c0ef35d61e8842005a330f47717cc7966
Yuri Haraguth <haraguths@gmail.com>
2017-11-24 00:14:56 -0200

    Implementa autenticacao

--- src/main/authOrApp.jsx
+++ src/main/authOrApp.jsx
@@ -18,6 +18,7 @@ class AuthOrApp extends Component {
 
     render() {
         const { user, validToken } = this.props.auth
+
         if(user && validToken) {
             axios.defaults.headers.common['authorization'] = user.token
             return <App>{this.props.children}</App>
